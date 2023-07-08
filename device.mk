3b24f68ac04ac459bec23d26dce4156ceff4c946
Killerpac <nilavasarkar@gmail.com>
2023-07-08 13:23:59 +0530

    nashc: fix build fcm and add dolby

--- device.mk
+++ device.mk
@@ -16,6 +16,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Dolby
+$(call inherit-product, hardware/dolby/dolby.mk)
+
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
