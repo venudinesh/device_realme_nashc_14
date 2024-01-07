1281391d298610145348717ba3dfc225ab61f153
Killerpac <nilavasarkar@gmail.com>
2024-01-07 14:18:38 +0530

    nashc: add more wakeup nodes **also fix extract script

--- extract-files.sh
+++ extract-files.sh
@@ -54,9 +54,6 @@ function blob_fixup {
         vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so)
             grep -q "libcamera_metadata_shim_nashc.so" "${2}" || "${PATCHELF}" --add-needed "libcamera_metadata_shim_nashc.so" "${2}"
             ;;
-        vendor/lib*/hw/vendor.mediatek.hardware.pq@2.13-impl.so)
-            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
-            ;;
         vendor/bin/mnld|\
         vendor/lib64/hw/android.hardware.sensors@2.X-subhal-mediatek.so|\
         vendor/lib*/libcam.utils.sensorprovider.so|\
@@ -65,7 +62,6 @@ function blob_fixup {
             ;;
         vendor/lib*/hw/vendor.mediatek.hardware.pq@2.13-impl.so)
             "$PATCHELF" --replace-needed "libutils.so" "libutils-v32.so" "$2"
-            "$PATCHELF" --add-needed "libshim_sensors.so" "$2"
             ;;
         vendor/lib*/libmtkcam_stdutils.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "$2"
