ff6f3308bc6f2a4b21805481697ee2eb3f734c25
Killerpac <nilavasarkar@gmail.com>
2024-01-09 09:57:26 +0530

    nashc: cleanup rootdir

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -7,8 +7,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE	   := fstab.mt6785_ramdisk
-LOCAL_MODULE_STEM  := fstab.mt6785
+LOCAL_MODULE	   := init.target.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.target.rc
@@ -40,18 +39,18 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.mt6781.rc
+LOCAL_MODULE       := init.mt6785.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.mt6781.rc
+LOCAL_SRC_FILES    := etc/init.mt6785.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.mt6781.usb.rc
+LOCAL_MODULE       := init.mt6785.usb.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.mt6781.usb.rc
+LOCAL_SRC_FILES    := etc/init.mt6785.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
@@ -72,19 +71,19 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.mt6781
+LOCAL_MODULE       := fstab.mt6785
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.mt6781
+LOCAL_SRC_FILES    := etc/fstab.mt6785
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.mt6781_ramdisk
-LOCAL_MODULE_STEM  := fstab.mt6781
+LOCAL_MODULE       := fstab.mt6785_ramdisk
+LOCAL_MODULE_STEM  := fstab.mt6785
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.mt6781
+LOCAL_SRC_FILES    := etc/fstab.mt6785
 LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)/system/etc
 include $(BUILD_PREBUILT)
 
